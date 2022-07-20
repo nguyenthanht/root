77d12174468cea27f56c781f420e25ab28864ce4
nguyenthanht <86815188+nguyenthanht@users.noreply.github.com>
2022-07-20 17:27:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h30 hẹn ok.',
+    messDesc: 'Tối nay  hẹn ok.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100026872742766' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink:  //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
